7a05bca3c8557cf3c346f9d4079452567e5a9c5c
srihariyati(●'◡'●) <sri.hr@mhs.unsyiah.ac.id>
2021-06-11 15:03:10 +0700

    add file 11juni 15.03

---
+++ resources/views/home.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>buatPlanner|Home</title>
+</head>
+    <!--css here-->
+    <!-- Favicons -->
+    <!--<link href="assets/img/icondapemas-1.png" rel="icon">
+    <link href="assets/img/icondapemas-1.png" rel="apple-touch-icon">-->
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+<body>
+    <div class="container d-flex">
+      <div class="contact-info mr-auto">
+        <i> Tugas MK PBW</i> 
+        <i> Sri Hariyati/Intan Malahayati</i> 
+      </div>
+    </div>  <!-- ======= Top Bar ======= -->
+  <div id="topbar" class="d-none d-lg-flex align-items-center fixed-top">
+  
+  </div>
+
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+    <h1 class="logo mr-auto"><a href="/">buatPlanner</a></h1>
+    <nav class="nav-menu d-none d-lg-block">
+      <ul>
+        <li class="active"><a href="index.html">Home</a></li>
+        <li><a href="index.html">Tutorial</a></li>
+        <li><a href="/mahasiswa">Contact Us</a></li>
+      </ul>
+      </nav><!-- .nav-menu -->
+    </div>
+  </header><!-- End Header -->
+
+  <section id="hero" class="d-flex align-items-center">
+      
+  </section><!-- End Hero -->
+</body>
+</html>
