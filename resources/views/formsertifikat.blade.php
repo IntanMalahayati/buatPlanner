8b26753687c5937d36b430fa7ba7e897335c55ff
srihariyati(●'◡'●) <sri.hr@mhs.unsyiah.ac.id>
2021-06-16 21:21:29 +0700

    Update formsertifikat.blade.php

--- resources/views/formsertifikat.blade.php
+++ resources/views/formsertifikat.blade.php
@@ -3,34 +3,50 @@
 @section('title', 'Sertifikat | buatPlanner')
 
 @section('container')
-<label> Nama Penerima :
-    <input id="name" type="text" placeholder="nama">
-</label>
-<br>
-<label> Penghargaann atas :
-<input id="tujuan" type="text" placeholder="tujuan">
-</label>
-<br>
-<label> Petinggi 1 :
-<input id="petinggi1" type="text" placeholder="petinggi1">
-</label>
-<br>
-<label> Status Petinggi 1 :
-<input id="statuspetinggi1" type="text" placeholder="statuspetinggi1" >
-</label>
-<br>
-<label> Petinggi 2 :
-<input id="petinggi2" type="text" placeholder="petinggi2">
-</label>
-<br>
-<label> Status Petinggi 2 :
-<input id="statuspetinggi2" type="text" placeholder="statuspetinggi2">
-</label>
-<br>
+<section class="page-section bg-white">
+    <div class="container px-4 px-lg-5">
+        <div class="row align-items-center justify-content-center text-center">
+            <div class="col-lg-8 align-self-end">
+                <h2 class="text-center mt-0">Isi Templatemu!</h2>
+                <hr class="divider" />
+            </div>
+        </div>
+        <div class="row gx-4 gx-lg-5 mt-5 justify-content-center mb-5">
+            <div class="col-lg-6 text-center">
+                <form>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="name" type="text" placeholder="Nama Penerima"/>
+                        <label>Nama Penerima</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="tujuan" type="text" placeholder="Penghargaan Atas"/>
+                        <label>Penghargaan Atas</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="petinggi1" type="text" placeholder="Petinggi 1"/>
+                        <label>Petinggi 1</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="statuspetinggi1" type="text" placeholder="Status Petinggi 1"/>
+                        <label>Status Petinggi 1</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="petinggi2" type="text" placeholder="Petinggi 2"/>
+                        <label>Petinggi 2</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="statuspetinggi2" type="text" placeholder="Status Petinggi 2"/>
+                        <label>Status Petinggi 2</label>
+                    </div>
+                </form>
+            </div>
 
-
-<canvas id="canvassertif" height="350px" width="500px" ></canvas>
-<br>
-<a href="#" id="download-btn">Download Sertifikat</a>
-<script src="/js/sertifikat.js"></script>
+            <div class="col-md-6 mb-3">
+                <canvas class="text-center" id="canvassertif" height="350px" width="500px" ></canvas><br>
+                <div class="text-center"><a  class="btn btn-primary btn-xl" href="#" id="download-btn">Download!</a>&nbsp;&nbsp;</div>
+                <script src="/js/sertifikat.js"></script>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
